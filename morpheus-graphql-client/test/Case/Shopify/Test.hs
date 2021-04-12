0cd83390b23ca10a56c25d8c482a027f96ea2518
Davit Nalchevanidze <d.nalchevanidze@gmail.com>
2021-04-12 12:00:59 +0200

    duplicate

--- morpheus-graphql-client/test/Case/Shopify/Test.hs
+++ morpheus-graphql-client/test/Case/Shopify/Test.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
